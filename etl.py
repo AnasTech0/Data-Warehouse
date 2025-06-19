d8a699bfc5a55244522b22c395a41fdd13c12168
Mohamed Anas M <161296508+AnasTech0@users.noreply.github.com>
2025-06-19 23:26:04 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,12 +4,14 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''This function is used to copy the values from s3 bucket to staging tables...'''
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''This function is used to insert values from the staging table into the created table...'''
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
